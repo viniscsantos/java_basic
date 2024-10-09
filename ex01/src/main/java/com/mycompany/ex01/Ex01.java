765acdde518f51a9675944f9e6752ca6cbabffda
viniscsantos <126025212+viniscsantos@users.noreply.github.com>
2024-10-09 20:08:32 -0300

    Primeiro commit

---
+++ ex01/src/main/java/com/mycompany/ex01/Ex01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ex01;
+
+/**
+ *
+ * @author vcsme
+ */
+public class Ex01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
